a775827e7627223c4a3a7574e6bf1df5f0549e48
harry <xun335610@163.com>
2023-10-27 17:35:35 +0800

    查找照片功能

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://127.0.0.1:8097' });
+const api = axios.create({ baseURL: 'https://www.aiavr.com/gallery/' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
