6f4173859bc1416ada8dc297da5b5c7321b44d00
harry <xun335610@163.com>
2023-10-30 13:04:23 +0800

    ssr

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -6,7 +6,7 @@
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        功能开发中,请期待
       </div>
 
       <q-btn
@@ -15,7 +15,7 @@
         text-color="blue"
         unelevated
         to="/"
-        label="Go Home"
+        label="返回首页"
         no-caps
       />
     </div>
